b1ad9d735ce75235e713904638f09e44919c0cfe
fikrilal <fikrimuzakky24@gmail.com>
2024-05-21 13:10:34 +0700

    feat: add colors, fonts, & dimensions library

---
+++ lib/_core/presentation/constants/colors.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class TextColors {
+  static const grey900 = Color(0xFF111928);
+  static const grey800 = Color(0xFF1F2A37);
+  static const grey700 = Color(0xFF374151);
+  static const grey600 = Color(0xFF4B5563);
+  static const grey500 = Color(0xFF6B7280);
+  static const grey400 = Color(0xFF9CA3AF);
+  static const grey300 = Color(0xFFD1D5DB);
+  static const grey200 = Color(0xFFE5E7EB);
+  static const grey100 = Color(0xFFF3F4F6);
+  static const grey50 = Color(0xFFF9FAFB);
+}
+
+class BrandColors {
+  static const brandPrimary950 = Color(0xFF122120);
+  static const brandPrimary900 = Color(0xFF263b39);
+  static const brandPrimary800 = Color(0xFF294642);
+  static const brandPrimary700 = Color(0xFF305550);
+  static const brandPrimary600 = Color(0xFF386962);
+  static const brandPrimary500 = Color(0xFF48847a);
+  static const brandPrimary400 = Color(0xFF63a094);
+  static const brandPrimary300 = Color(0xFF8abeb2);
+  static const brandPrimary200 = Color(0xFFb6d9d0);
+  static const brandPrimary100 = Color(0xFFdbece7);
+  static const brandPrimary50 = Color(0xFFf4f9f8);
+  static const brandSecondary = Color(0xFFFBF289);
+  static const brandAccent = Color(0xFFFFE3E3);
+}
+
+class AppColors {
+  static const successColor = Color(0xFF219653);
+  static const warningColor = Color(0xFFEB5757);
+  static const errorColor = Color(0xFFF2994A);
+  static const linkColor = Color(0xFF2F80ED);
+}
---
+++ lib/_core/presentation/constants/dimensions.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppSpacing {
+  static double get xxs => 2.0.w;
+  static double get xs => 4.0.w;
+  static double get sm => 8.0.w;
+  static double get md => 16.0.w;
+  static double get lg => 24.0.w;
+  static double get xl => 32.0.w;
+  static double get xxl => 40.0.w;
+}
+
+class AppTextSize {
+  static double get xxsmall => 10.0.sp;
+  static double get xsmall => 12.0.sp;
+  static double get small => 14.0.sp;
+  static double get medium => 15.0.sp;
+  static double get mediumToLarge => 16.0.sp;
+  static double get large => 18.0.sp;
+  static double get xlarge => 20.0.sp;
+}
